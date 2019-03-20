7ef277ce33031e55173a68d7004821dd425abd48
bsky <git@bsky.moe>
2019-03-21 05:52:04 +0900

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -2,11 +2,12 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 const WebpackBar = require('webpackbar');
 
 module.exports = {
   entry: './src/index.js',
-  output: { filename: '[name].[hash].js', path: path.join(__dirname, 'dist') },
+  output: { filename: 'bundle.js', path: path.join(__dirname, 'dist') },
   module: {
     rules: [
       {
@@ -18,6 +19,8 @@ module.exports = {
           }
         ]
       },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.vue$/, loader: 'vue-loader' },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -27,6 +30,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({ title: 'KnzkLive', favicon: 'static/favicon.ico' }),
+    new VueLoaderPlugin(),
     new CopyPlugin([{ from: 'static', to: 'static' }]),
     new WebpackBar({ profile: true, reporter: 'profile' })
   ]
